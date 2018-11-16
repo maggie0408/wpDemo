ad22184e91463177f4097b57ac5029158ef5c6ab
maggie0408 <1210590780@qq.com>
2018-11-16 18:06:56 +0800

    update

---
+++ src/main.js
@@ -0,0 +1,2 @@
+const demo=require('../public/js/demo.js');
+const demoCss=require('../public/css/demo.css');
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+var webpack=require('webpack');
+var path=require('path');
+var htmlWebpackPlugin=require('html-webpack-plugin');
+var ExtractTextPlugin=require('extract-text-webpack-plugin');
+var CopyWebpackPlugin=require('copy-webpack-plugin');
+var CleanWebpackPlugin=require('clean-webpack-plugin');
+
+module.exports={
+    mode: 'production',
+
+    entry:{
+        index:__dirname+"/src/main.js"
+    },
+
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:"js/bundle.js",
+        publicPath:"/dist/"       
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                loader:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader"
+                })
+            },
+            {
+                test:/\.(png|jpg|gif|svg)$/,
+                loader:'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+            }
+        ]
+    },
+    
+    plugins:[
+        new ExtractTextPlugin("style.css"),
+        /*
+        new CopyWebpackPlugin([
+            {
+                from:__dirname+'/public',
+                to:__dirname+'/dist'
+            }
+        ]),
+        */
+        new CleanWebpackPlugin(
+            ['dist'],
+            {
+                root:__dirname,
+                verbose:true,
+                dry:false
+            }
+        ),
+
+        new htmlWebpackPlugin({
+            filename:'html/index.html',
+            template:'./public/html/index.html'
+        })
+    ],
+
+    node:{
+        fs:"empty",
+        net:"empty"
+    },
+
+    target: 'web',
+
+    watch:true
+}
+
