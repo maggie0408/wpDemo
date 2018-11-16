01e8e795dd569cbadaeeae8096768ea59cf42fff
maggie0408 <1210590780@qq.com>
2018-11-16 18:03:42 +0800

    update

---
+++ public/js/demo.js
@@ -0,0 +1,7 @@
+module.exports=(function(){
+    alert("hello");
+    var start=document.getElementById("start");
+    var img=document.getElementById("img");
+    var oImg=require('../img/1.png');
+    img.src=oImg;
+})();
